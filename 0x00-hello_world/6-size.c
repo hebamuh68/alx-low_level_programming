45faa6ee4539e7e9003dff9ba0f0fdab65254926
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-13 12:32:45 -0800

    dn

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    char charType;
+    int intType;
+    long int longType;
+    long long int longlongType;
+    float floatType;
+
+    printf("Size of a char: %zu byte(s)\n", sizeof(intType));
+    printf("Size of an int: %zu byte(s)\n", sizeof(charType));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+    return 0;
+}
