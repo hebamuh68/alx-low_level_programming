42be53921736498be4a1e905d36e725a9d07df6b
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-13 09:26:05 -0800

    dn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * Description:  C program that prints exactly "Programming is like building a multilingual puzzle,
+ * main - Entry point to the program
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
