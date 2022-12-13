e332ba5b5ea5272f1a2754e5ebebb97f624c79e8
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-13 09:23:27 -0800

    dn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+	/**
+	 * Description:  C program that prints exactly "Programming is like building a multilingual puzzle,
+	 */
+
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
