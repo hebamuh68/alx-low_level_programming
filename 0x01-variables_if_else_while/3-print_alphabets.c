ee3af6d2dc846f469db0c4181a92317a7d9da12c
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-14 00:39:52 -0800

    dn

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -14,7 +13,7 @@ int main(void)
 	{
 		putchar(ch);
 	}
-    for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
