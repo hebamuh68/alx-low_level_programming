b0da9e9876be7a889fb4576da938b4d52b4d6f36
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-16 21:34:15 +0200

    dn

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
 #include "main.h"
+
 /**
- * Write a function that checks for uppercase character.
-*/
+ * _isupper - Entry point
+ *
+ * @c: c is a variable name
+ *
+ * Return: Always 0 (Success)
+ */
+
 int _isupper(int c)
 {
-if (c >= 97 || c <= 122)
-{
-_putchar(0);
-}
-else
-{
-_putchar(0);
-}
+if (c >= 97 && c <= 122)
+return (0);
+if (c >= 65 && c <= 90)
+return (1);
+
+return (0);
 }
