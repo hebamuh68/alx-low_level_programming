08189c457687303a01df4f1df300c2f16a425c7d
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-17 00:27:42 +0200

    dn

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
+	int largest;
 
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > a && b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	if (a > b && a > c)
+	{
+	largest = a;
+	}
+	else if (b > a && b > c)
+	{
+	largest = b;
+	}
+	else if (a == b)
+	{
+	largest = a;
+	}
+	else
+	{
+	largest = c;
+	}
 
-return (largest);
+	return (largest);
 }
