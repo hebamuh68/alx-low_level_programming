84a0ac5aa6dcdc1f8e0f88e17e60cf03f4fa24e3
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-16 06:24:28 +0200

    dn

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,10 @@
  * Returns 1 if c is lowercase
  * Returns 0 otherwise
 */
-int _islower(int c)
+int _isalpha(int c)
 {
-    if (islower(c))
-    {
-        return (0);
-    }
-    return (1);
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
 }
