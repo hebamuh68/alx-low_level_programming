8279a21444ced4aeb9e5ddd059d96ab6b89782f1
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-16 21:28:44 +0200

    dn

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Write a function that checks for uppercase character.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' || c <= 'Z')
+{
+_putchar(1);
+}
+else
+{
+_putchar(0);
+}
+}
