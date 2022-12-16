85932a81875007cc714b8eb01eb67700b52b340b
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-17 00:05:03 +0200

    dn

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * debug postitive_or_negative()
+*/
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
