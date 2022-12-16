9ac74ac232eab8192540804dd2d0fcb3535afafb
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-17 00:29:30 +0200

    dn

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * debug postitive_or_negative()
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
+
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
