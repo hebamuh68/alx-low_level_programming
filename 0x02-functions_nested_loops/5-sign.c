a4c1dde16da0959caea01851927752ffa97a875d
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-16 06:43:38 +0200

    dn

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
-#include <limits.h>
 #include "main.h"
-
+#include <limits.h>
 /**
-  * print_sign - Entry point
- *
- * @n: is a variable name
- *
- * Return: Always 0 (Success)
- */
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
-{
-_putchar('-');
-return (-1);
-}
+    if (n > 0)
+    {
+        _putchar("+");
+        return(1);
+    }
+    else if (n < 0)
+    {
+        _putchar("-");
+        return(-1);
+    }
+    else
+    {
+        _putchar("");
+        return(0);
+    }
 }
