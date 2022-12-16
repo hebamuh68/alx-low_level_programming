8278e3b266aac3e62a12c4a6926dee15722d6604
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2022-12-16 06:29:35 +0200

    dn

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
-*/
-
+ * _isalpha -Entry point
+ *@c: The character to print
+ *
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
-    if (isalpha(c))
-    {
-        return(1);
-    }
-    return(0);
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
 }
