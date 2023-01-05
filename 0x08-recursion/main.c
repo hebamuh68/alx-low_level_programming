2c4b91a398850ded7d30674ebc472a7601442c93
hebamuh68 <hebatuallah.20349064@compit.aun.edu.eg>
2023-01-05 16:43:36 +0200

    dn

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,16 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+int factorial(int n){
+
+    if ( n < 0){
+        return -1;
+    }
+
+    if (n == 1 || n == 0){
+        return 1;
+    }
+    
+    return n * factorial(n-1);
+}
+
 int main(void)
 {
-    int n;
+    int r;
 
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);
     return (0);
 }
